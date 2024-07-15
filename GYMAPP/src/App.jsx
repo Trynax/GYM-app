075d5c881c09ebcbcc3ffe8c5645b9d9df7b3ec9
Trynax <kandoso111@gmail.com>
2024-07-15 09:36:02 +0100

    file setup

---
+++ GYMAPP/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+
+  return (
+    <>
+<header>
+  <h1 className='text-lg text-green-300'>Hello World</h1>
+</header>
+    </>
+  )
+}
+
+export default App
